7389f816943c37c805c4c293f23ba45df2f63390
Lior Shahverdi <lior@Liors-MacBook-Pro.local>
2016-07-22 00:17:58 -0400

    added home and post templates come back to angular later moving onto node

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
 
 app.controller('MainCtrl',[
 	'$scope',
-	function($scope){
+	'posts',
+	function($scope, posts){
 		$scope.pageTitle = "Thinkster IO MEAN Stack App";
 
-		$scope.posts = [
-		  {title: 'post 1', upvotes: 5},
-		  {title: 'post 2', upvotes: 2},
-		  {title: 'post 3', upvotes: 15},
-		  {title: 'post 4', upvotes: 9},
-		  {title: 'post 5', upvotes: 4}
-		];
+		$scope.posts = posts.posts;
 
 		$scope.addPost = function(){
 		  if(!$scope.title || $scope.title === '') { return; }
 		  $scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes: 0
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
 		   });
 			$scope.title = '';
 			$scope.link = '';
@@ -26,5 +53,23 @@ app.controller('MainCtrl',[
 
 		$scope.incrementUpvotes = function(post) {
 		  post.upvotes += 1;
+		};
+	}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
 		};
 	}]);
